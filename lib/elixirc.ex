3ad0ba3ca584961d71d91a91a1733094d1dd93fe
Ethan Williams <ewilli23@nd.edu>
2019-03-05 12:14:14 -0500

    Relax USER command validation

--- lib/elixirc.ex
+++ lib/elixirc.ex
@@ -59,7 +59,7 @@ defmodule Elixirc do
             {nick, Elixirc.Responses.response_nickspec(mapping[:params]), "elixIRC"}
         end
       "USER" ->
-         result = Elixirc.Validate.validate mapping[:params], [{:pattern, "^[^ :,]+$"}, {:matches, "0"}, {:matches, "*"}, {:pattern, "^[^:,]+$"}]
+         result = Elixirc.Validate.validate mapping[:params], [{:pattern, "^[^ :,]+$"}, {:pattern, ".*"}, {:pattern, ".*"}, {:pattern, "^[^:,]+$"}]
          case result do
            {:ok, _} ->
              [head|tail] = mapping[:params]
