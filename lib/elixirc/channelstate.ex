96ca05317657bface50f4d10f15aaf70fbfd117b
AndrewLitteken <andrew.litteken@gmail.com>
2019-03-05 12:05:29 -0500

    updated mode for mapset and channels

--- lib/elixirc/channelstate.ex
+++ lib/elixirc/channelstate.ex
@@ -34,4 +34,34 @@ defmodule Elixirc.ChannelState do
 		Agent.stop(channel)
 	end
 
+	def change_channel_mode(channel, modestring, op \\ "add") when modestring != "" do
+		case modestring do
+			"+"<>rest ->
+				change_channel_mode(channel, rest, "add")
+			"-"<>rest ->
+				change_channel_mode(channel, rest, "sub")
+			nil ->
+				modes = Agent.get(channel, &Map.get(&1, :modes))
+				{:return, List.to_string(Enum.map(MapSet.to_list(modes), fn n -> to_string(n) end))}
+			_ ->
+				case op do
+					"add" ->
+						modes = Agent.get(channel, &Map.get(&1, :modes))
+						modes = MapSet.put(modes, String.at(modestring, 0))
+						Agent.update(channel, &Map.put(&1, :modes, modes))
+					"sub" ->
+						modes = Agent.get(channel, &Map.get(&1, :modes))
+						modes = if MapSet.member?(modes, String.to_atom(String.at(modestring, 0))) do
+							MapSet.delete(modes, String.to_atom(String.at(modestring, 0)))
+						end
+						Agent.update(channel, &Map.put(&1, :modes, modes))
+				end
+				change_channel_mode(channel, String.slice(modestring, 1, String.length(modestring)), op)
+		end
+	end
+
+	def change_user_mode(_, modestring, _) when modestring == "" do
+		{:ok, nil}
+	end
+
 end
